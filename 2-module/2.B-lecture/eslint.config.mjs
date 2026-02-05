8c64514915c4aa1b7ac5b610b79dc1665806a750
James Strahan <j.strahan@everway.com>
2026-02-05 20:24:38 +0000

    checking on husky

--- 2-module/2.B-lecture/eslint.config.mjs
+++ 2-module/2.B-lecture/eslint.config.mjs
@@ -1,35 +1,27 @@
+import js from '@eslint/js';
 import globals from 'globals';
-import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import eslintPluginPlaywright from 'eslint-plugin-playwright';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
+//import eslintPluginPlaywright from 'eslint-plugin-playwright';
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { ignores: ['package-lock.json', 'playwright-report/**', 'test-results/**'] },
-  { files: ['**/*.ts', '**/*tsx'] },
+export default defineConfig([
   {
+    ignores: ['package-lock.json', 'playwright-report/**', 'test-results/**'],
+  },
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     languageOptions: {
       globals: globals.node,
-    },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    rules: {
-      '@typescript-eslint/explicit-function-return-type': 'error',
-    },
-  },
-  eslintPluginPlaywright.configs['flat/recommended'],
-  {
-    rules: { 'no-console': 'error', 'no-var': 'error' },
-    settings: {
-      playwright: {
-        globalAliases: {
-          test: ['setup'],
-        },
+      parserOptions: {
+        warnOnUnsupportedTypeScriptVersion: true,
       },
     },
+    rules: {
+      'no-console': 'error',
+      'no-var': 'error',
+    },
+    // extends: [js.configs.recommended, tsPlugin.configs.recommended],
   },
-  eslintPluginPrettierRecommended,
-];
+]);
--- 2-module/2.B-lecture/tests-examples/demo-todo-app.spec.ts
+++ 2-module/2.B-lecture/tests-examples/demo-todo-app.spec.ts
@@ -18,7 +18,9 @@ test.describe('New Todo', () => {
 
     // Create 1st todo.
     await newTodo.fill(TODO_ITEMS[0]);
-    await newTodo.press('Enter');
+
+    //var localStorage = '';
+    //console.log('Test');
 
     // Make sure the list only has one todo item.
     await expect(page.getByTestId('todo-title')).toHaveText([TODO_ITEMS[0]]);
